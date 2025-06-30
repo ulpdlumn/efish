bad5291efe68ba5153d26f7e27e3f99c5c60b62b
davide <davide@delcont.eu>
2025-06-30 16:59:12 -0500

    added

---
+++ 250630_demoHV.py
@@ -0,0 +1,29 @@
+import serial
+import time
+from PS300 import *
+
+# Connect to power supply
+ps = PS300(port='COM6')
+
+# Basic operation
+ps.set_voltage(-10)      # Set output to 5000V
+assert ps.get_last_error() == 0
+ps.set_current_limit(-3e-4)  # Set current limit to 0.3mA (max 525ua)
+ps.set_current_trip(-5e-4)  #TODO check that the current has to be negative here
+assert ps.get_last_error() == 0
+ps.high_voltage_on()      # Enable output
+assert ps.get_last_error() == 0
+
+# Monitor parameters
+for ii in range(10):
+    print(f"Voltage: {ps.get_output_voltage()} V")
+    print(f"Current: {ps.get_output_current() * 1e6} μA")
+
+ps.high_voltage_off()      # Enable output
+
+
+# Save configuration
+# ps.save_configuration(1)
+
+# Cleanup
+ps.close()
