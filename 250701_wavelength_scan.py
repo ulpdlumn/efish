3962af5e6cbd4c5772d8ebb9a84351411c64d41e
davide <davide@delcont.eu>
2025-07-02 09:10:40 -0500

    added

---
+++ 250701_wavelength_scan.py
@@ -0,0 +1,200 @@
+'''This script demonstrate how to use some automation scripts to automatically collect an energy scan of the laser '''
+
+USELASER = False
+
+
+import seeOutside
+from lc4 import *
+import PS300 as ps
+import pg575v2 as pg
+if USELASER: import opotek2 as opo
+
+import pickle
+import numpy as np
+
+
+import time
+import threading
+from pathlib import Path
+import os, configparser
+import matplotlib.pyplot as plt
+
+### TODO:
+# pressure sensor channel #4: just get the average, not the waveform
+
+
+allowedMDepth = [25e6, 12.5e6, 5e6, 1.25e6, 5e5, 125e3, 5e4, 125e2, 5e3, 1250]
+allowedtdiv   = [100, 200, 500]  
+
+
+notes = "_em570_sensormoved"    # additional notes
+EMCorrFac = 1/5 # energy meter correction factor [V/W]
+LaserFreq = 10  # [Hz]
+waitingtime = 3 # time after tuning before starting the acquisition
+aqtime = 5  # acquisition time for each condition 
+wstep = 5   # wavelength scan step 
+reps = 10
+chs = [1,2,4]
+
+wavelengths = np.arange(565, 585, 5)
+
+saveloc = "__media__/"  # location where to save files 
+QS_DELAY_US = 50    # (35–160 µs allowed)
+
+
+# Q-Smart keep-alive -------------------------------------------------
+if USELASER:
+    def _keep_alive():
+        while keep_running:
+            opo.laser_error_check(SYSTEM)   # lightweight ping
+            time.sleep(4)                   # I think the timeout is 10 s
+
+
+# ----
+def oscilloscope(chs:list=chs, tdiv : float = 10e-9, mdepth : int = allowedMDepth[-2], init : bool = False, segments = 5):
+    """Test single-shot acquisition (sequence off)."""
+    scope = MauiScope("192.168.8.223", debug=True)
+    try:
+        scope._log('[FUNC] Single acquisition ')
+        scope.buzz()
+        if init:
+            scope.sequence(enable=True, segments=segments)
+
+            for ii in range(1, 5):
+                scope.enable_channel(ii, False)
+                scope.enable_trace(ii, False)
+            for ch in chs:
+                scope.enable_channel(ch, True)
+                scope.enable_trace(ch, True)
+
+            # scope.set_tdiv(tdiv)
+            scope.set_mdepth(mdepth)
+            # scope.set_mdepth(1_000)
+            scope._log('INIT COMPLETED...')
+        
+        scope.single(timeout=15, forceTr=False)
+
+        collection = []
+        for ch in chs:
+            t, y = scope.get_waveform(ch, max_points=mdepth*segments, with_time=True)
+            print(f"Sequence: captured {len(y)} samples - shuold have been {mdepth*segments}")
+            collection.append(y)
+    finally:
+        scope.close()
+    return t, collection
+
+# ─── 0. Desired conditions ───────────────────────────────────────────────────
+
+SYSTEM = 0                  # single-system setup
+WAVELENGTH_NM = 580.0       # desired output
+
+if True:
+        # allowedMDepth[-3]
+        t, y = oscilloscope(init = 0==0, mdepth=250, tdiv=10e-9, segments=4, )
+        import matplotlib.pyplot as plt
+        fig, ax= plt.subplots(3, 1, sharex=True)
+        for ii, ch in enumerate(chs):
+            ax[ii].plot(t, y[ii])
+        plt.draw();plt.pause(.001)
+
+
+        input(aaaa)
+
+
+if USELASER:
+    # ─── 1. Initialise ───────────────────────────────────────────────────────────
+    print("System init...")
+    opo.system_init(system_index=SYSTEM)
+
+    # ─── 2. Home all motors ──────────────────────────────────────────────────────
+    print("Homing motors...")
+    opo.motor_home(SYSTEM)      # homes OPO & UV axes
+
+    # ─── 3. Select the Signal range ──────────────────────────────────────────────
+    print("Figuring out which configuration to use...")
+    CONFIG_SIGNAL = opo.pick_config(WAVELENGTH_NM)  # figure it out from ini file
+    low, high = opo.system_select_config(CONFIG_SIGNAL, SYSTEM)
+
+    # ─── 4. Tune to 580 nm ───────────────────────────────────────────────────────
+    print(f"Tuning to {WAVELENGTH_NM} nm...")
+    assert low <= WAVELENGTH_NM <= high
+    opo.system_tune(WAVELENGTH_NM, SYSTEM)
+
+    # ─── 5. Turn the flash-lamp ON (internal trigger) ────────────────────────────
+    print(f"Turning flashlamps on...")
+    opo.laser_flash_lamp(1, 0, SYSTEM)
+
+    # ─── 6. Set output energy via Q-switch delay ─────────────────────────────────
+    print(f"Setting qswitch delay to {QS_DELAY_US}...")
+    opo.laser_qswitch_delay(QS_DELAY_US, SYSTEM)
+
+    # ─── 7.0 Wait some time before qswitch  ──────────────────────────────────────
+    print(f"Waiting 10 seconds...")
+    keep_running = True
+    threading.Thread(target=_keep_alive, daemon=True).start()
+    time.sleep(10)
+    keep_running = False            # stop keep-alive thread
+
+    # ─── 7.1 Enable the Q-switch (start lasing) ───────────────────────────────────
+    print(f"Enabling qswitch...")
+    opo.laser_qswitch(1, SYSTEM)
+
+# ─── Run beam for 10 s ───────────────────────────────────────────────────────
+collection = []
+for ii, wavelength in enumerate( wavelengths ):
+        print(f"Running the system for 10 seconds at {wavelength}...")
+        # check if the configuration can be kept the same
+        assert low <= wavelength <= high
+        if USELASER:
+            opo.system_tune(wavelength, SYSTEM)
+            keep_running = True
+            threading.Thread(target=_keep_alive, daemon=True).start()
+        time.sleep(waitingtime)   # wait some seconds before actually starting to collet data
+
+#.        if ii==0: input("check scope")
+        # here collect data from the oscilloscope
+        t, y = oscilloscope(ch =4, tdiv = aqtime/10, init = ii==0)
+        collection.append([QS_DELAY_US, wavelength,  ])
+
+        print(f"Average energy at {wavelength} n: {y.mean()}")
+
+
+collection = np.array(collection)
+## saving the results
+with open(f'{saveloc}energy_scan_QS{QS_DELAY_US}{notes}.pkl', 'wb') as fi:
+    pickle.dump(collection, fi)
+
+
+if USELASER:
+    keep_running = False            # stop keep-alive thread    # maybe if can go one indentation less
+    # ─── 8. Disable the Q-switch ────────────────────────────────────────────────
+    print(f"Disabling qswitch...")
+    keep_running = False            # stop keep-alive thread    # maybe if can go one indentation less
+    opo.laser_qswitch(0, SYSTEM)
+
+    # ─── 9. Turn the flash-lamp OFF ──────────────────────────────────────────────
+    print(f"Stopping flashlamps...")
+    opo.laser_flash_lamp(0, 0, SYSTEM)
+
+    # ─── 10. Park motors (reduces next-boot init time) ───────────────────────────
+    print(f"Parking motors...")
+    opo.motor_park(SYSTEM)
+
+    # ─── 11. Close ports & release resources ─────────────────────────────────────
+    print(f"Closing the system...")
+    opo.system_close(SYSTEM)
+
+
+## plot the reuslts
+if True:
+    fig, ax= plt.subplots()
+    ax.plot(collection[:,1], collection[:,2] / EMCorrFac / LaserFreq, 'sr', label=f'Energy');
+    ax.set_xlabel("Wavelength [nm]")
+    ax.set_ylabel("Average pulse energy [J]")
+    ax.set_title(f"Average pulse energy vs wavelength\nat QS delay: {QS_DELAY_US} us")
+    plt.legend(frameon=False); 
+    ax.grid()
+    plt.draw();plt.pause(.001)
+    plt.savefig( f'{saveloc}energy_scan_QS{QS_DELAY_US}{notes}.png')
+    
+    
